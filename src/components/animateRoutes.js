d381c9b562104a1c936ee1dce54f73e14a499f26
Paul Morris <creative2012@hotmail.com>
2023-03-04 00:03:22 +0000

    setup gitpages

--- src/components/animateRoutes.js
+++ src/components/animateRoutes.js
@@ -8,7 +8,7 @@ function AnimatedRoutes() {
     const location = useLocation();
 
   return (
-    <AnimatePresence intial={false}>
+    <AnimatePresence>
           <Routes location={location} key={location.pathname}>
             <Route path="/" element={<LandingPage/>} />
             <Route path="about" element={<About/>} />
