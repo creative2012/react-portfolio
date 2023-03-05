5aee80063fe35be66646c8f0d8acc8c50511070e
Paul Morris <creative2012@hotmail.com>
2023-03-05 19:54:53 +0000

    changed project page to use JSON

--- src/components/pages/projects/index.js
+++ src/components/pages/projects/index.js
@@ -1,140 +1,10 @@
 import React from 'react';
 import ProjectBox from './components/projectBox';
-import image1 from './images/aritasn.jpg';
-import image2 from './images/dateLounge.png';
-import image3 from './images/readme.png';
-import image4 from './images/weatherapp.png';
-import image5 from './images/blackJack.png';
-import image6 from './images/portfolio.png';
-import placeHolder from './images/matrix.jpg';
+import projects from './projects.json';
 import './style.css';
 
 function Projects() {
     let t = 0.4;
-    //array of projects with details
-    const projects = [
-        {
-            id: "pp_one",
-            url: "https://github.com/creative2012/css-artisan",
-            title: "CSS Artisan",
-            description: "A free resource.",
-            skills:
-                [
-                    "sp devicon-javascript-plain colored",
-                    "sp devicon-css3-plain-wordmark colored"
-                ],
-            backgroundImage: `url("${image1}")`,
-        },
-        {
-            id: "pp_two",
-            url: "#",
-            title: "The Date Lounge",
-            description: "A Dating Platform.",
-            skills:
-                [
-                    "sp devicon-laravel-plain-wordmark colored",
-                    "sp devicon-react-original-wordmark colored",
-                    "sp devicon-photoshop-plain colored"
-                ],
-            backgroundImage: `url("${image2}")`,
-        },
-        {
-            id: "pp_three",
-            url: "https://github.com/creative2012/Professional-README-Generator",
-            title: "Readme Generator",
-            description: "NodeJs command line App",
-            skills:
-                [
-                    "sp devicon-nodejs-plain colored"
-                ],
-            backgroundImage: `url("${image3}")`,
-        },
-        {
-            id: "pp_four",
-            url: "https://github.com/creative2012/weather-dashboard",
-            title: "Weather App",
-            description: "5 day forcast App using openweather API",
-            skills:
-                [
-                    "sp devicon-html5-plain-wordmark colored",
-                    "sp devicon-css3-plain-wordmark colored",
-                    "sp devicon-jquery-plain-wordmark colored"
-                ],
-            backgroundImage: `url("${image4}")`,
-        },
-        {
-            id: "pp_five",
-            url: "https://github.com/creative2012/BlackJack",
-            title: "Black Jack",
-            description: "Simple Browser Game",
-            skills:
-                [
-                    "sp devicon-html5-plain-wordmark colored",
-                    "sp devicon-css3-plain-wordmark colored",
-                    "sp devicon-javascript-plain colored"
-                ],
-            backgroundImage: `url("${image5}")`,
-        },
-        {
-            id: "pp_six",
-            url: "https://github.com/creative2012/Bootstrap-Portfolio",
-            title: "1st Portfolio",
-            description: "Made during my time at Edx bootcamp",
-            skills:
-                [
-                    "sp devicon-html5-plain-wordmark colored",
-                    "sp devicon-css3-plain-wordmark colored",
-                    "sp devicon-bootstrap-plain-wordmark colored"
-                ],
-            backgroundImage: `url("${image6}")`,
-        },
-        {
-            id: "pp_seven",
-            url: "",
-            title: "Comming Soon",
-            description: "",
-            skills:
-                [
-                    
-                ],
-            backgroundImage: `url("${placeHolder}")`,
-        },
-        {
-            id: "pp_eight",
-            url: "",
-            title: "Comming Soon",
-            description: "",
-            skills:
-                [
-                    
-                ],
-            backgroundImage: `url("${placeHolder}")`,
-        },
-        {
-            id: "pp_nine",
-            url: "",
-            title: "Comming Soon",
-            description: "",
-            skills:
-                [
-                    
-                ],
-            backgroundImage: `url("${placeHolder}")`,
-        },
-        {
-            id: "pp_ten",
-            url: "",
-            title: "Comming Soon",
-            description: "",
-            skills:
-                [
-                    
-                ],
-            backgroundImage: `url("${placeHolder}")`,
-        }
-    ];
-
-
 
     return (
 
@@ -142,7 +12,7 @@ function Projects() {
             {
             projects.map((project, i) => {
                 if(i % 2 === 0) t = t += 0.1;
-                
+
                 return (<ProjectBox
                     timer={t}
                     key={i}
